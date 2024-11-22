15302704d093ae249669b63c36bf521867d6ba5d
Misbahul Muttaqin <misbahulmuttaqin395@gmail.com>
2024-11-22 12:52:49 +0700

    navbar update

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -37,12 +37,12 @@ const Navbar = () => {
 
     return (
         <header className={`fixed top-0 left-0 w-full z-50 flex justify-center items-center py-4 ${scroll ? "translate-y-0" : "-translate-y-20 hover:translate-y-0"} transition-all duration-300`}>
-            <nav className='flex gap-3 items-center bg-slate-800/70 backdrop-blur-md px-6 py-1.5 rounded-xl'>
+            <nav className='flex gap-3 items-center bg-slate-800/70 backdrop-blur-md sm:px-6 px-4 py-1.5 rounded-xl'>
                 <Link href={'/'}>
                     <Image src={'/images/logo.jpg'} alt='logo' width={50} height={50} className='size-9 bg-cover rounded-full' />
                 </Link>
                 {NAVBAR_LINK.map((link) => (
-                    <Link href={link.href} key={link.id} className='px-6 py-2.5 grid place-items-center hover:bg-white hover:bg-opacity-10 rounded-lg'>
+                    <Link href={link.href} key={link.id} className='sm:px-6 px-3 sm:py-2.5 py-1 sm:text-lg text-xs grid place-items-center hover:bg-white hover:bg-opacity-10 rounded-lg'>
                         <p className='text-white font-bold text-sm'>{link.title}</p>
                     </Link>
                 ))}
